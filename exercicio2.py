3fc8b050e04f2ff4c0d36ed5b13bdab5591a4c5d
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2022-03-20 10:53:10 -0300

    Delete exercicio2.py

--- exercicio2.py
+++
@@ -1,6 +0,0 @@
-'''
-Faça um programa que peça um número e, então, mostre
-a mensagem 'O número informado foi [número].
-'''
-numero = int(input('Digite um número qualquer aqui: '))
-print(f'O número informado foi {numero}.')
