c4d64132402d8f43be722ec0b50fcb24e8c41e03
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2022-03-20 10:53:44 -0300

    Delete exercicio5.py

--- exercicio5.py
+++
@@ -1,6 +0,0 @@
-'''
-Faça um programa que converta metros para centímetros.
-'''
-metros = float(input('Digite uma medida em metros (pode ser um número quebrado): '))
-centimetros = metros * 60
-print(f'A quantidade em centimetros da medida fornecida em metros é de {centimetros} centímetros.')
