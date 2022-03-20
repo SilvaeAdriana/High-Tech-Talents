fa44751c2d3425ff3e19f1635334f2d08863b25c
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2022-03-20 10:53:22 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,8 +0,0 @@
-'''
-Faça um programa que peça dois números e imprima a soma.
-'''
-print('Aqui você pode somar dois números a sua escolha!')
-a = int(input('Digite o primeiro número a ser somado: '))
-b = int(input('Digite o segundo número a ser somado: '))
-soma = a + b
-print(f'A soma dos dois números apresentados é de {soma}.')
